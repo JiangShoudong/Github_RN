851fb30938741d5278d2ddfaec3a20369ca27b7e
jiangshoudong <jiangshoudong@wecash.net>
2019-01-29 15:48:20 +0800

    init project

---
+++ Github/js/navigator/AppNavigator.js
@@ -0,0 +1,21 @@
+/**
+ * AppNavigator.js
+ *
+ * Created by 姜守栋 on 2019/1/29.
+ * Copyright © 2018年 jiangshoudong. All rights reserved.
+ */
+
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    View,
+} from 'react-native';
+
+export default class AppNavigator extends Component {
+    render() {
+        return (
+            <View style={{flex: 1}}>
+            </View>
+        );
+    }
+}
